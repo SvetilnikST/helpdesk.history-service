9201baf78515b5155652fa3eaabec92c502f9064
SvetilnikST <svetilnik.stezhkina@gmail.com>
2022-05-12 18:15:37 +0300

    keycloak config

--- src/main/java/innowise/zuevsky/helpdesk/historyservice/controller/HistoryController.java
+++ src/main/java/innowise/zuevsky/helpdesk/historyservice/controller/HistoryController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.RolesAllowed;
+import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,20 +17,27 @@ import java.util.List;
 @RequiredArgsConstructor
 public class HistoryController implements IHistoryController {
 
-    @GetMapping("/message")
-    public String test() {
-        return "message";
+    @GetMapping("/user")
+    @RolesAllowed({"USER"})
+    public String getProduct(Principal principal) {
+        return "Response from History Service, User Id:" + principal.getName();
     }
-
+    @GetMapping("/employee")
+    @RolesAllowed({"EMPLOYEE"})
+    public String getProductOne(Principal principal) {
+        return "EMPLOYEE :" + principal.getName();
+    }
 
     @Override
     @GetMapping("/{historyId}")
+    @RolesAllowed({"EMPLOYEE"})
     public HistoryDto getHistory(Long historyId) {
         return null;
     }
 
     @Override
     @GetMapping("/{ticketId}")
+    @RolesAllowed({"EMPLOYEE"})
     public List<HistoryDto> getHistorysByTicketId() {
         return new ArrayList<>();
     }
